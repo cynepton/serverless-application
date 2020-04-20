3da0c3c3321b3f5a78617934d64d0cb120d6eb91
cynepton <aderonmuisaac@gmail.com>
2020-04-20 12:41:55 +0100

    Updated client/src/config.ts
    Added Auth0 clientID and Domain

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,7 +4,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-34f3-2rh.auth0.com',            // Auth0 domain
+  clientId: 'TA7yEN8ESw77HrU1SpN3I8JhM9VHQ9CJ',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
